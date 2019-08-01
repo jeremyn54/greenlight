a972edab9f4793668bb31fdbef4ccecf7d453d00
jeremyn54 <52896218+jeremyn54@users.noreply.github.com>
2019-08-01 13:11:27 +1200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -119,7 +119,7 @@ module Greenlight
     # DEFAULTS
 
     # Default branding image if the user does not specify one
-    config.branding_image_default = "https://raw.githubusercontent.com/bigbluebutton/greenlight/master/app/assets/images/logo_with_text.png"
+    config.branding_image_default = "https://raw.githubusercontent.com/jeremyn54/greenlight/master/app/assets/images/logo_with_text.png"
 
     # Default primary color if the user does not specify one
     config.primary_color_default = "#467fcf"
